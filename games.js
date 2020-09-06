205d9974c2b1921fc6a6ce2e7fd88a4c8e9fc930
Violeta Rodriguez <55716688+violeta1391@users.noreply.github.com>
2020-09-06 15:18:07 -0300

    games.js termine la funcion letsPlay

--- games.js
+++ games.js
@@ -1,1 +1,48 @@
 
+// Constantes
+
+
+const playButton = document.getElementById('playButton');
+
+
+// FUNCION PLAY 
+
+playButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    funLetsPlay();
+})
+
+function funLetsPlay() {
+
+    let answerRight = "nose";
+    let chance = 4;
+    var success = false;
+    var answer;
+
+    while (chance > 0 && success == false) {
+        chance--;
+        answer = prompt("You have " + chance + " try to hit");
+        if (answer == answerRight) {
+            alert("Congratulations!! your answer is correct. You are a true fan of cats!!");
+            success = true;
+        }
+        else {
+            switch (chance) {
+                case 3:
+                    alert("Clue one: It's not the ears");
+                    break;
+                case 2:
+                    alert("Clue two: It's not the whiskersista")
+
+                    break;
+                case 1:
+                    alert("Clue three: Neither are their legs")
+                    break;
+                default:
+                    alert("You lost your last chance, try again!");
+            }
+        }
+    }
+}
+
+
